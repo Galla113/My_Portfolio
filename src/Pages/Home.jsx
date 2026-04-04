4f68541c516f0778eb917689fd38fd46df3b738e
Galla113 <guillermo.arevalo1113@gmail.com>
2026-04-04 11:46:10 -0600

    feat: add home.css and style scrollbar with hover interaction.

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,13 @@
 import Intro from "../Components/IntroAnimation/Intro";
 import Header from "../components/Header";
+import "./home.css";
 
 export default function MainLayout() {
   return (
     <>
       <Intro />
       <Header>
-        <div style={{ height: "100vh", overflowY: "auto", background: "#000000", color: "gray"}}>
+        <div className="content">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
           <br />
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
